56255aec46d07234fa023596cc4ef66824d05482
Niveditha Rajesh <niveditharn12@gmail.com>
2025-02-15 20:54:50 +0530

    added shop-products

--- server/server.js
+++ server/server.js
@@ -1,38 +1,42 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const authRouter = require('./routes/auth/auth-routes');
-const adminProductsRouter = require('./routes/admin/products-routes');
+/*mongoose.connect('mongodb+srv://fathimajinshabp:AJaynYic8jyTNXJj@cluster0.ejidr.mongodb.net/'
+).then( ()=>console.log('MongoDB connected') ).catch((error) => console.log(error));
+*/
 
 
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const shopProductsRouter = require("./routes/shop/products-routes");
 
-mongoose.connect(''
+mongoose.connect('mongodb+srv://fathimajinshabp:AJaynYic8jyTNXJj@cluster0.ejidr.mongodb.net/'
 ).then( ()=>console.log('MongoDB connected') ).catch((error) => console.log(error));
 
-const app = express() //
+
+const app = express();
 const PORT = process.env.PORT || 5000;
 
-
 app.use(
-    cors({
-        origin: 'http://localhost:5173',
-        methods: ['GET','POST','DELETE','PUT'],
-        allowedHeaders:[
-            "Content-Type",
-            "Authorization",
-            "Cache-Control",
-            "Expires",
-            "Pragma",
-        ],
-        credentials : true
-    })
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
 );
 
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/admin/products', adminProductsRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
+app.use("/api/shop/products", shopProductsRouter);
 
-
-app.listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
